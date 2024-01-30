a3b90d31f5298da60c3a4cf4a1a0efbfe0509f3d
harmonious-parrot-66629 <153278035+harmonious-parrot-66629@users.noreply.github.com>
2024-01-30 02:38:26 +0000

    added instruments object to JS

--- projects/week-6-project/source/scripts/script.js
+++ projects/week-6-project/source/scripts/script.js
@@ -1,3 +1,35 @@
-console.log(
-    "Hello world"
-);
+const modal = document.getElementById("myModal");
+const button = document.getElementById("modalButton");
+const span = document.getElementsByClassName("close")[0];
+
+button.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+const instruments = {
+    "guitar": {
+        "title": "guitar title",
+        "description": "guitar description",
+        "image": "https://placehold.co/600x400/000000/FFF"
+    },
+    "drums": {
+        "title": "drums title",
+        "descriptiom": "drums description",
+        "image": "https://placehold.co/600x400/000000/000"
+    },
+    "keyboard": {
+        "title": "keyboard title",
+        "description": "keyboard description",
+        "image": "https://placehold.co/600x400/000000/ff0"
+    },
+}
+console.log(instruments)
+console.log(instruments.guitar.title)
